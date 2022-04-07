2b9c75a1f640b6f4ecb1ce437f833ad2fa4b1c56
zoemckenzie <zmckenzie82@gmail.com>
2022-04-07 09:03:33 -0700

    Add incrementation to count

--- src/application/FXMLController.java
+++ src/application/FXMLController.java
@@ -2,10 +2,14 @@ package application;
 
 import java.net.*;
 import java.util.*;
+import javafx.event.*;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 
 public class FXMLController implements Initializable {
+    private int count = 0;
+    private int num = 0;
+
     @FXML
     Button myButton;
 
@@ -14,8 +18,30 @@ public class FXMLController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        myButton.setText("I AM A BUTTON");
-        myLabel.setText("Yo!");
+        myButton.setText("Count");
+        myButton.setOnAction(event);
+        myLabel.setText("0");
     }
+
+    EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent e) {
+            if (num < 20) {
+                num = count;
+                num++;
+                myLabel.setText("" + num);
+                update(num);
+            } else {
+                myLabel.setText("You've exceeded\n your limit");
+            }
+
+
+        }
+
+        public void update(int n) {
+            count = n;
+        }
+    };
+
 
 }
